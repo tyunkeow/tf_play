c3c79d4a274c5ea66a6a589907ec4e0d384c76d8
jharms <mail@jan-harms.com>
2020-02-19 21:18:55 +0100

    play with jupyter images

--- upyter.sh
+++ upyter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -p 8888:8888 --rm -v `pwd`:/app tf-play jupyter notebook --ip 0.0.0.0 --no-browser --allow-root
+docker run -it -p 8888:8888 --rm -v `pwd`:/work tf-play jupyter notebook --ip 0.0.0.0 --no-browser --allow-root
